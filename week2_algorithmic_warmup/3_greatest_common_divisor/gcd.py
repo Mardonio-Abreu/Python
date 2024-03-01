fec87c283bc2814434c22e14d1ba9bb356d9e882
Mardonio Abreu <mardonio.abreu@protonmail.com>
2024-03-01 10:39:07 -0600

    Second week

--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,34 +1,21 @@
-def naive_gcd(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
-                return current_gcd
-            
 def gcd(a, b):
-    if (a == 0):
-        mcd = b
-    elif (b == 0):
-        mcd = a
-    else:
-        a = max(a, b)
-        b = min(a, b)
-    
-    while (a % b != 0):
+    while (b != 0):
         c = a
         a = b
-        b = (c % b)
+        b = (c % a)
+        print(c, a)
 
-    return a / b
+        if (b == 0):
+            return a
+            
+
+print(gcd(18, 35))
+
+
+
 
 
 
     
 
 
-if __name__ == "__main__":
-    #a, b = map(int, input().split())
-    a, b = 24, 4
-    print(naive_gcd(a, b))
-    print(gcd(a, b))
