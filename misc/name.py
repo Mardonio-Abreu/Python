5665c531d481a538fafae8d3c48b62e62c5798c0
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2025-08-20 12:47:12 -0600

    N queens refactored to show both solutions of 4 quuens
    Signed-off-by: Mardonio-Abreu <mardonio.abreu@protonmail.com>

--- misc/name.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def name():
-    
-    list1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    list2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    print(random.choice(list2) + random.choice(list2) + random.choice(list2) + '-' + random.choice(list1) + random.choice(list1) + random.choice(list1))
-    
-name()
--- misc/what_is_the_number.py
+++
@@ -1,5 +0,0 @@
-list = range(9, 99, 1)
-
-for i in list:
-    if (((i-2)%3 == 0) and ((i-3)%4 == 0) and ((i-4)%5 == 0)):
-        print(i)
